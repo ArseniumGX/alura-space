8562926cdce82ce0049e5dcaee7bfc7343538b77
José Pereira Macedo <arseniumgx@gmail.com>
2023-08-07 21:34:48 -0300

    feat(aula5): finalizando o projeto

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -2,13 +2,21 @@ import style from "./Gallery.module.scss";
 import { Tags } from "./Tags/Tags";
 import photos from "../../moch/photos.json";
 import { Cards } from "./Cards/Cards";
+import { useEffect, useState } from "react";
 
 export function Gallery() {
+  const [items, setItems] = useState(photos);
+
+  const tags = [...new Set(photos.map((photo) => photo.tag))];
+
+  const photosFilter = (tag) =>
+    setItems(photos.filter((photo) => photo.tag === tag));
+
   return (
     <section className={style.gallery}>
       <h2>Navegue pela galeria</h2>
-      <Tags />
-      <Cards photos={photos} />
+      <Tags tags={tags} photosFilter={photosFilter} setItems={setItems} />
+      <Cards photos={items} />
     </section>
   );
 }
